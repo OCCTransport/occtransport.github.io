d24885c8ada330690a93370d9bb2eb9f297092bb
bennettcmw <stuffins396@yahoo.com>
2026-09-09 17:57:13 -0400

    emergency update

--- js/alerts.js
+++ js/alerts.js
@@ -1,6 +1,6 @@
 const ALERT = {
   active: true,
-  text: 'Rosh Hashanah Break (9/11-9/13): OCCT is running an Alternate reduced schedule on all routes. Regular weekday and weekend service resumes Monday, 9/14. Late Nite service will not operate.',
+  text: 'Wednesday, Sep 9: Expect Severe Delays. Refer to SPOT or ETA Transit Announcements.      Rosh Hashanah Break (9/11-9/13): OCCT is running an Alternate reduced schedule on all routes. Regular weekday and weekend service resumes Monday, 9/14. Late Nite service will not operate.',
   link: { href: 'routeschedule.html', label: 'View schedules' }
 };
 
